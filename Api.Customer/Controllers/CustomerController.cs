924d47e4867d5fd4bd84e2048247c45a7061e5e5
Liam Grainger <liam.grainger@thinkmoneygroup.com>
2019-07-27 11:36:42 +0100

    Initial check in - new web api for customers

---
+++ Api.Customer/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("customerId")]
+        public ActionResult<string> Get(int customerId)
+        {
+            return Ok(new {customerId});
+        }
+    }
+}
