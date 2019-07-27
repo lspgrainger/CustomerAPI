b5f6235ea3ccd0512720c6afc7f665ba56da3a48
Liam Grainger <liam.grainger@thinkmoneygroup.com>
2019-07-27 12:08:25 +0100

    Add swagger

--- Api.Customer/Startup.cs
+++ Api.Customer/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Api.Customer
 {
@@ -26,23 +20,40 @@ namespace Api.Customer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc($"v{Configuration["Version"]}", new Info
+                {
+                    Title = Configuration["Name"],
+                    Version = Configuration["Version"]
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+
+            ConfigSwagger(app);
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private void ConfigSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger(options => { options.RouteTemplate = "swagger/{documentName}/swagger.json"; });
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = "swagger";
+                options.SwaggerEndpoint($"v{Configuration["Version"]}/swagger.json",
+                    $"{Configuration["Name"]} {Configuration["Version"]}");
+            });
         }
     }
 }
